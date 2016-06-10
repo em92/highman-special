0516675c06716b4f89c4e08087b55c29ad1c1390
Eugene M <eugene.m92@gmail.com>
2016-06-10 23:31:43 +0500

    - теперь основной функционал через /shuffle/ - добавил проверку вводимого стим ид - исправил методы ввода стим ид - основной функционал теперь выводит ники

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var ql = require("./quakelive.js");
 
+var httpd_port = require("./cfg.json").httpd_port;
 var app = express();
 
 var convertDiscordIdsToArray = function(s) {
@@ -10,7 +11,7 @@ var convertDiscordIdsToArray = function(s) {
 	return data;
 };
 
-app.get('/:gametype/:discord_ids', function (req, res) {
+app.get('/shuffle/:gametype/:discord_ids', function (req, res) {
 	ql.shuffle(req.params.gametype, convertDiscordIdsToArray(req.params.discord_ids), function(result) {
 		res.setHeader("Content-Type", "application/json");
 		res.send(result);
@@ -18,21 +19,19 @@ app.get('/:gametype/:discord_ids', function (req, res) {
 });
 
 app.get('/map/:discord_id/:steam_id', function (req, res) {
-	res.setHeader("Content-Type", "application/json");
-	if (typeof(ql.d2s[req.params.discord_id]) == 'undefined') {
-		ql.setSteamId(req.params.discord_id, req.params.steam_id);
-		res.send({ok: true});
-	} else {
-		res.send({ok: false, message: 'idi na hui'});
-	}
+	ql.setSteamIdPrimary(req.params.discord_id, req.params.steam_id, function(result) {
+		res.setHeader("Content-Type", "application/json");
+		res.send(result);
+	});
 });
 
 app.get('/force_map/:discord_id/:steam_id', function (req, res) {
-	ql.setSteamId(req.params.discord_id, req.params.steam_id);
-	res.setHeader("Content-Type", "application/json");
-	res.send({ok: true});
+	ql.setSteamId(req.params.discord_id, req.params.steam_id, function(result) {
+		res.setHeader("Content-Type", "application/json");
+		res.send(result);
+	});
 });
 
-app.listen(3330, function () {
+app.listen(httpd_port, function () {
 	console.log("privet, pupsik :3");
 });
--- quakelive.js
+++ quakelive.js
@@ -2,25 +2,91 @@ var http = require('http');
 var fs = require('fs');
 var extend = require('util')._extend;
 var d2s = require('./d2s.json');
+var steamApiKey = require('./cfg.json').steamApiKey;
 
 var NO_ERROR = 0;
 var INVALID_GAMETYPE = 1;
 var INVALID_PLAYER_COUNT = 2;
+var INVALID_STEAM_ID = 3;
+var STEAM_ID_ALREADY_SET = 4;
 var ERROR_LIST = [
 	'no error',
 	'invalid gametype',
-	'invalid player count (must be even)'
+	'invalid player count (must be even)',
+	'invalid steam id',
+	'steam id already set. use /force_map method'
 ];
 
-var setSteamId = function(discordId, steamId) {
-	d2s[discordId] = steamId;
-	fs.writeFile("./d2s.json", JSON.stringify(d2s));
+var GetPlayerSummaries = function(steamids, done, fuck) {
+	var params = {
+		host: "api.steampowered.com",
+		port: 80,
+		path: '/ISteamUser/GetPlayerSummaries/v0002/?key=' + steamApiKey + '&steamids=' + steamids
+	};
+	
+	http.get(params, function(response) {
+		var data = "";
+		response.on("data", function(chunk) {
+			data += chunk;
+		});
+		response.on("end", function() {
+			try {
+				data = JSON.parse(data);
+				done(data);
+			} catch(yourself) {
+				fuck(yourself);
+			}
+		});
+	}).on('error', fuck);
 };
+
+var setSteamId = function(discordId, steamId, done) {
+	// если что-то пойдет не так
+	var fuck = function(err) {
+		done({
+			"ok": false,
+			"error_code": -1,
+			"error_msg": err.toString()
+		});
+	};
+	
+	// сначала проверяем валидности steamId
+	GetPlayerSummaries(steamId, function(data) {
+		if (data.response.players.length == 0) {
+			done({
+				"ok": false,
+				"error_code": INVALID_STEAM_ID,
+				"error_msg": ERROR_LIST[INVALID_STEAM_ID]
+			});
+		} else {
+			d2s[discordId] = steamId;
+			fs.writeFile("./d2s.json", JSON.stringify(d2s), function(err) {
+				if (err) fuck(err);
+				done({ok: true});
+			});
+		}
+	}, fuck);
+};
+
+
+var setSteamIdPrimary = function(discordId, steamId, done) {
+	if (typeof(d2s[discordId]) == 'undefined') {
+		setSteamId(discordId, steamId, done);
+	} else {
+		done({
+			"ok": false,
+			"error_code": STEAM_ID_ALREADY_SET,
+			"error_msg": ERROR_LIST[STEAM_ID_ALREADY_SET]
+		});
+	}
+};
+
 
 var shuffle = function(gametype, playerList, done) {
 	
 	var teamsize = parseInt(Object.keys(playerList).length / 2);
 	var playercount = Object.keys(playerList).length;
+	var steamNames = {};
 	
 	// метод получения рейтинга
 	var getRatings = function(done, fuck) {
@@ -36,10 +102,12 @@ var shuffle = function(gametype, playerList, done) {
 				data = JSON.parse(data);
 				playerList.forEach(function(item, i, a) {
 					a[i].elo = 1500;
+					a[i].steamname = "noname";
 					data.players.forEach(function(jtem) {
 						if (jtem.steamid == item.steamid) {
 							if (typeof(jtem[gametype]) != 'undefined') {
 								a[i].elo = jtem[gametype].elo;
+								a[i].steamname = steamNames[item.steamid];
 							}
 						}
 					});
@@ -149,19 +217,44 @@ var shuffle = function(gametype, playerList, done) {
 				player.steamid = '0';
 			} else {
 				player.steamid = d2s[player.discordid];
+				steamNames[player.steamid] = "noname";
 			}
 			return player;
 		});
 		
-		// если не получится получить рейтинги
-		var onGetRatingsError = function(error) {
-			console.error('http error in getRatings: ' + error.message);
+		// для получения ников со стима
+		var GetPlayerSummariesCallback = function(data) {
+			
+			data.response.players.forEach(function(player) {
+				var name = player.personaname;
+				name = name.split("^0").join("");
+				name = name.split("^1").join("");
+				name = name.split("^2").join("");
+				name = name.split("^3").join("");
+				name = name.split("^4").join("");
+				name = name.split("^5").join("");
+				name = name.split("^6").join("");
+				name = name.split("^7").join("");
+				steamNames[player.steamid] = name;
+			});
+			
+			// если не получится получить рейтинги
+			var onGetRatingsError = function(error) {
+				console.error('http error in getRatings: ' + error.message);
+				getRatings(makeTeams, onGetRatingsError);
+			}
+			
 			getRatings(makeTeams, onGetRatingsError);
-		}
+		};
+		
+		var onGetPlayerSummariesError = function(error) {
+			console.error("error in GetPlayerSummaries: " + error.message);
+			GetPlayerSummaries(Object.keys(steamNames), GetPlayerSummariesCallback, onGetPlayerSummariesError);
+		};
 		
 		// поехали
-		getRatings(makeTeams, onGetRatingsError);
-
+		GetPlayerSummaries(Object.keys(steamNames), GetPlayerSummariesCallback, onGetPlayerSummariesError);
+		
 	} else {
 		done({
 			"ok": false,
@@ -173,6 +266,7 @@ var shuffle = function(gametype, playerList, done) {
 
 };
 
+
 module.exports.shuffle = shuffle;
 module.exports.setSteamId = setSteamId;
-module.exports.d2s = d2s;
+module.exports.setSteamIdPrimary = setSteamIdPrimary;
