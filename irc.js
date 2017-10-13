992c5f153a104dcdca31f0ad06cd97b8b1aea5f0
Eugene Molotov <em92@users.noreply.github.com>
2017-10-13 15:03:50 +0500

    removed #ctfpickup from watching

--- irc.js
+++ irc.js
@@ -1,16 +1,14 @@
 var irc = require('irc');
 var client = new irc.Client('irc.quakenet.org', 'eugene_irc_bot', {
   stripColors: true,
-  channels: ['#tdmpickup', '#ctfpickup']
+  channels: ['#tdmpickup']
 });
 
 var status = {
-  "#ctfpickup": "n/a",
   "#tdmpickup": "n/a"
 }
 
 var bot_names = {
-  "#ctfpickup": ["pq-Doom", "pq-Lucy", "pq-Orbb", "pq-Mynx"],
   "#tdmpickup": ["TDMBot"]
 }
 
@@ -61,7 +59,6 @@ client.addListener('topic', function (channel, topic, nick, message) {
   // [ tdm [0/8] 2v2 [0/4] skilled [0/8] ] [ http://www.esreality.com/post/2873410/hoq-tdm-spring-season-catchup-round-1/ ]
   // CTF: 4on4 [ 0 / 8 ] 5on5 [ 0 / 10 ] Skilled [ 0 / 8 ] -/-
   switch(channel) {
-    case "#ctfpickup": topic = topic.split(" -/-")[0]; break;
     case "#tdmpickup": topic = topic.startsWith("[ ") ? topic.split("[ ")[1].split(" ] ")[0] : topic; break;
     default: return;
   }
